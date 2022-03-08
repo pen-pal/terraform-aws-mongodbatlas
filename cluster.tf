40845b212a234b7bbb70ba2c16881f2ec444f886
pen-pal <unameme@protonmail.com>
2022-03-09 00:34:15 +0545

    update cluster information

--- cluster.tf
+++ cluster.tf
@@ -5,7 +5,7 @@
 resource "mongodbatlas_cluster" "cluster" {
   project_id                  = mongodbatlas_project.project.id
   name                        = var.cluster_name
-  provider_name               = var.instance_type == "M0" ? "TENANT" : local.cloud_provider
+  provider_name               = var.instance_type == "M0" ? "TENANT" : var.cloud_provider
   backing_provider_name       = var.instance_type == "M0" ? "AWS" : ""
   provider_region_name        = var.region
   provider_instance_size_name = var.instance_type
@@ -23,8 +23,11 @@ resource "mongodbatlas_cluster" "cluster" {
   auto_scaling_disk_gb_enabled = var.instance_type == "M0" ? false : var.auto_scaling_disk_gb_enabled
 
   #cluster tier scaling
-  auto_scaling_compute_enabled                    = var.auto_scaling_compute_enabled
-  auto_scaling_compute_scale_down_enabled         = var.auto_scaling_compute_scale_down_enabled
-  provider_auto_scaling_compute_min_instance_size = var.provider_auto_scaling_compute_min_instance_size
-  provider_auto_scaling_compute_max_instance_size = var.provider_auto_scaling_compute_max_instance_size
+  auto_scaling_compute_enabled                    = var.instance_type == "M0" ? false : var.auto_scaling_compute_enabled
+  auto_scaling_compute_scale_down_enabled         = var.instance_type == "M0" || var.auto_scaling_compute_enabled == false ? false : var.auto_scaling_compute_scale_down_enabled
+  provider_auto_scaling_compute_min_instance_size = var.instance_type == "M0" || var.auto_scaling_compute_enabled == false ? "" : var.provider_auto_scaling_compute_min_instance_size
+  provider_auto_scaling_compute_max_instance_size = var.instance_type == "M0" || var.auto_scaling_compute_enabled == false ? "" : var.provider_auto_scaling_compute_max_instance_size
+
+  #encryption
+  encryption_at_rest_provider = var.encryption_at_rest_provider
 }
