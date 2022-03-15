c73bfcf7f7f79c9090b24382c782a58635450aa3
pen-pal <unameme@protonmail.com>
2022-03-15 23:35:03 +0545

    enable encryption based on either kms created key or hardcoded value

--- encryption.tf
+++ encryption.tf
@@ -1,65 +1,14 @@
 ########################################
-## AWS Roles
-########################################
-resource "aws_iam_role_policy" "policy" {
-  count = var.enabled == true ? 1 : 0
-  name  = "mongodb_atlas_setup_policy"
-  role  = aws_iam_role.role[0].id
-
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-		"Action": "*",
-		"Resource": "*"
-      }
-    ]
-  }
-  EOF
-
-  depends_on = [
-    aws_iam_role.role
-  ]
-}
-
-resource "aws_iam_role" "role" {
-  count = var.enabled == true ? 1 : 0
-  name  = "mongodb_setup_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${mongodbatlas_cloud_provider_access_setup.setup_only[0].aws_config[0].atlas_aws_account_arn}"
-      },
-      "Action": "sts:AssumeRole",
-      "Condition": {
-        "StringEquals": {
-          "sts:ExternalId": "${mongodbatlas_cloud_provider_access_setup.setup_only[0].aws_config[0].atlas_assumed_role_external_id}"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
-
-########################################
 ## MongoDB Atlas
 ########################################
 
 resource "mongodbatlas_encryption_at_rest" "encryption" {
-  count      = var.enabled == true ? 1 : 0
-  project_id = mongodbatlas_project.project.id
+  count      = var.create && var.encryption_enabled ? 1 : 0
+  project_id = mongodbatlas_project.project[0].id
 
   aws_kms_config {
-    enabled                = var.enabled
-    customer_master_key_id = var.customer_master_key_id
+    enabled                = var.encryption_enabled
+    customer_master_key_id = var.customer_master_key_id == "" ? aws_kms_key.default[0].key_id : var.customer_master_key_id
     region                 = var.region #atlas region (this region should have your kms key in AWS)
     role_id                = mongodbatlas_cloud_provider_access_authorization.auth_role[0].role_id
   }
@@ -71,8 +20,8 @@ resource "mongodbatlas_encryption_at_rest" "encryption" {
 }
 
 resource "mongodbatlas_cloud_provider_access_setup" "setup_only" {
-  count         = var.enabled == true ? 1 : 0
-  project_id    = mongodbatlas_project.project.id
+  count         = var.create && var.encryption_enabled ? 1 : 0
+  project_id    = mongodbatlas_project.project[0].id
   provider_name = var.cloud_provider
 
   depends_on = [
@@ -81,8 +30,8 @@ resource "mongodbatlas_cloud_provider_access_setup" "setup_only" {
 }
 
 resource "mongodbatlas_cloud_provider_access_authorization" "auth_role" {
-  count      = var.enabled == true ? 1 : 0
-  project_id = mongodbatlas_project.project.id
+  count      = var.create && var.encryption_enabled ? 1 : 0
+  project_id = mongodbatlas_project.project[0].id
   role_id    = mongodbatlas_cloud_provider_access_setup.setup_only[0].role_id
 
   aws {
