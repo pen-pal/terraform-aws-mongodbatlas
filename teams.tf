7a0bb552f3b10fd162a8725902b39d0855e35834
pen-pal <unameme@protonmail.com>
2022-03-15 23:44:58 +0545

    update teams

--- teams.tf
+++ teams.tf
@@ -3,16 +3,8 @@
 ###########################################
 
 resource "mongodbatlas_teams" "team" {
-  for_each  = var.teams
+  for_each  = var.create ? var.teams : {}
   org_id    = var.org_id
   name      = each.key
   usernames = each.value.users
-
-  #org_id    = var.org_id
-  #name      = "DevOps"
-  #usernames = ["manish@altada.com"]
-
-  #depends_on = [
-  #  mongodbatlas_org_invitation.invite
-  #]
 }
