e3219f2dd7f2dbbef262e09c1d761bbd0b72c824
pen-pal <61139563+pen-pal@users.noreply.github.com>
2022-03-31 16:55:26 +0545

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "mongodb/mongodbatlas"
       version = "~> 1.3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-    aws = "~>4.0.0"
   }
 
   required_version = ">=0.12"
